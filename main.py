8d397cd284a81a94156af03455bdc64b15e99aa2
9898956556464546 <123912288+9898956556464546@users.noreply.github.com>
2023-01-30 18:23:31 -0500

    Actualizar main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,116 @@
+def on_button_multiplayer_a_pressed(player2):
+    global projectile
+    projectile = sprites.create_projectile_from_sprite(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . 4 4 4 4 4 . . . . . . . . . 
+                    . 4 4 4 1 1 4 4 . . . . . . . . 
+                    . 4 4 4 4 1 4 4 . . . . . . . . 
+                    . 4 4 4 4 4 4 4 . . . . . . . . 
+                    . 4 1 4 4 4 4 4 . . . . . . . . 
+                    . 4 4 1 4 4 4 4 . . . . . . . . 
+                    . . 4 4 4 4 4 . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        mp.get_player_sprite(mp.player_selector(mp.PlayerNumber.ONE)),
+        50,
+        -50)
+mp.on_button_event(mp.MultiplayerButton.A,
+    ControllerButtonEvent.PRESSED,
+    on_button_multiplayer_a_pressed)
+
+projectile: Sprite = None
+mp.set_player_sprite(mp.player_selector(mp.PlayerNumber.ONE),
+    sprites.create(img("""
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . f . . . . . . .
+        . . . . . . . f f f . . . . . .
+        . . . . . . f f 2 f f . . . . .
+        . . . . . f f 2 f 2 f f . . . .
+        . . . . f f 2 f f f 2 f f . . .
+        . . . f f 2 f f . f f 2 f f . .
+        . . . f 2 f f . . . f f 2 f . .
+        . . . f f f . . . . . f f f . .
+        . . . 5 4 5 . . . . . 5 4 5 . .
+        . . . . 5 . . . . . . . 5 . . .
+        . . . 5 4 5 . . . . . 5 4 5 . .
+        . . . . 5 . . . . . . . 5 . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+    """),
+        SpriteKind.player))
+mp.set_player_sprite(mp.player_selector(mp.PlayerNumber.TWO),
+    sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . f . . . . . . . 
+                . . . . . . . f f f . . . . . . 
+                . . . . . . f f 8 f f . . . . . 
+                . . . . . f f 8 f 8 f f . . . . 
+                . . . . f f 8 f f f 8 f f . . . 
+                . . . f f 8 f f . f f 8 f f . . 
+                . . . f 8 f f . . . f f 8 f . . 
+                . . . f f f . . . . . f f f . . 
+                . . . 5 4 5 . . . . . 5 4 5 . . 
+                . . . . 5 . . . . . . . 5 . . . 
+                . . . 5 4 5 . . . . . 5 4 5 . . 
+                . . . . 5 . . . . . . . 5 . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.player))
+mp.set_player_sprite(mp.player_selector(mp.PlayerNumber.THREE),
+    sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . f . . . . . . . 
+                . . . . . . . f f f . . . . . . 
+                . . . . . . f f 7 f f . . . . . 
+                . . . . . f f 7 f 7 f f . . . . 
+                . . . . f f 7 f f f 7 f f . . . 
+                . . . f f 7 f f . f f 7 f f . . 
+                . . . f 7 f f . . . f f 7 f . . 
+                . . . f f f . . . . . f f f . . 
+                . . . 5 4 5 . . . . . 5 4 5 . . 
+                . . . . 5 . . . . . . . 5 . . . 
+                . . . 5 4 5 . . . . . 5 4 5 . . 
+                . . . . 5 . . . . . . . 5 . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.player))
+mp.set_player_sprite(mp.player_selector(mp.PlayerNumber.FOUR),
+    sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . f . . . . . . . 
+                . . . . . . . f f f . . . . . . 
+                . . . . . . f f 4 f f . . . . . 
+                . . . . . f f 4 f 4 f f . . . . 
+                . . . . f f 4 f f f 4 f f . . . 
+                . . . f f 4 f f . f f 4 f f . . 
+                . . . f 4 f f . . . f f 4 f . . 
+                . . . f f f . . . . . f f f . . 
+                . . . 5 4 5 . . . . . 5 4 5 . . 
+                . . . . 5 . . . . . . . 5 . . . 
+                . . . 5 4 5 . . . . . 5 4 5 . . 
+                . . . . 5 . . . . . . . 5 . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.player))
+mp.move_with_buttons(mp.player_selector(mp.PlayerNumber.ONE), 100, 90)
+mp.move_with_buttons(mp.player_selector(mp.PlayerNumber.TWO), 100, 90)
+mp.move_with_buttons(mp.player_selector(mp.PlayerNumber.THREE), 100, 90)
+mp.move_with_buttons(mp.player_selector(mp.PlayerNumber.FOUR), 100, 90)
+
 def on_forever():
     pass
 forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,24 +1,24 @@
 mp.onButtonEvent(mp.MultiplayerButton.A, ControllerButtonEvent.Pressed, function (player2) {
-    mySprite = sprites.create(img`
+    projectile = sprites.createProjectileFromSprite(img`
         . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . 4 4 4 . . . . . . . . . . . 
-        . 4 4 5 5 4 . . . . . . . . . . 
-        . 4 4 4 5 4 . . . . . . . . . . 
-        . 4 4 4 4 4 . . . . . . . . . . 
-        . . 4 4 4 . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
+        . . 4 4 4 4 4 . . . . . . . . . 
+        . 4 4 4 1 1 4 4 . . . . . . . . 
+        . 4 4 4 4 1 4 4 . . . . . . . . 
+        . 4 4 4 4 4 4 4 . . . . . . . . 
+        . 4 1 4 4 4 4 4 . . . . . . . . 
+        . 4 4 1 4 4 4 4 . . . . . . . . 
+        . . 4 4 4 4 4 . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Projectile)
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), 100, -60)
 })
-let mySprite: Sprite = null
+let projectile: Sprite = null
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -91,10 +91,10 @@ mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player))
-mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One), 100, 100)
-mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two), 100, 100)
-mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Three), 100, 100)
-mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Four), 100, 100)
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One), 100, 90)
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two), 100, 90)
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Three), 100, 90)
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Four), 100, 90)
 forever(function () {
 	
 })
